211bd85025b819fe51a44a39a614bf8ea7e597f1
a-gbatie <ange.batie@gmail.com>
2021-01-03 16:06:04 -0800

    1/3/2020 submit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,42 @@
 class UsersController < ApplicationController
-    def show
-        @user = User.find(params[:id])
+    before_action :find_user, only: [:show, :edit, :update, :destroy]
+
+    def show 
     end
 
     def index
         @users = User.all
     end
 
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.create(users_params)
+        redirect_to @user
+    end
+
+    def edit
+    end
+
+    def update
+        @user.update(users_params)
+        redirect_to @user
+    end
+
+    def destroy
+        @user.destroy
+        redirect_to root_path
+    end
+
+private
+
+    def find_user
+        @user = User.find(params[:id])
+    end
+
+    def users_params
+        params.require(:user).permit(:name, :age, :hometown)
+    end
 end
